cd7c0480151e186b651c3a6fe5cf254a66d99ffe
Scott McClaugherty <gamefreak4321@gmail.com>
2011-02-25 17:07:05 -0500

    Made proximity zooms use new proximity system.

--- Resources/Scripts/Modules/Proximity.lua
+++ Resources/Scripts/Modules/Proximity.lua
@@ -44,49 +44,24 @@ end
 --[TEMP, SCOTT] these functions use a temporaty technique until we have a more efficient one
 
 function GetClosestHostile(subject)
-	local subjectId = subject.physics.object_id
-	local nearest = nil
-	local dist = 0
-	local pos = subject.physics.position
-
-	for id, other in pairs(scen.objects) do
-		if subject.ai.owner ~= other.ai.owner
-		and other.base.attributes.hated == true
-		and subjectId ~= id then
-			local tempDist = hypot2(pos, other.physics.position)
-
-			if tempDist < dist
-			or nearest == nil then
-				dist = tempDist
-				nearest = other
-			end
-		end
-	end
-
-	return nearest, dist
+    local closest = subject.proximity.closestHostile
+    local dist = subject.proximity.closestHostileDistance
+    if closest ~= nil then
+        return closest, dist
+    else
+        return GetFurthestObject(subject)
+    end
 end
 
 
 function GetClosestObject(subject)
-	local subjectId = subject.physics.object_id
-	local nearest = nil
-	local dist = 0
-	local pos = subject.physics.position
-
-	for id, other in pairs(scen.objects) do
-		if other.base.attributes.hated == true
-		and subjectId ~= id then
-			local tempDist = hypot2(pos, other.physics.position)
-
-			if tempDist < dist
-			or nearest == nil then
-				dist = tempDist
-				nearest = other
-			end
-		end
-	end
-
-	return nearest, dist
+    local closest = subject.proximity.closest
+    local dist = subject.proximity.closestDistance
+    if closest ~= nil then
+        return closest, dist
+    else
+        return GetFurthestObject(subject)
+    end
 end
 
 
